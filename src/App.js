5e04aa5d37e4976e59f217afcc3bcf44adaca32c
NathanDuggal <39681791+NathanDuggal@users.noreply.github.com>
2023-02-11 01:07:59 -0500

    grid

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+// import Grid from './Grid';
+import Grid from '@mui/material/Grid';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+
+const Item = function Item(props) {
+  const onClick = event => {
+    console.log(props.index);
+  }
+
+  return <div className='item' onClick={onClick}>{props.index}</div>
+}
+
+function select(e) {
+    // access to e.target here
+    console.log("here");
+}
 
 function App() {
   return (
@@ -17,6 +36,18 @@ function App() {
         >
           Learn React
         </a>
+        <br></br>
+        <div className='gridBox'>
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={0} columns={8}>
+              {Array.from(Array(28)).map((_, index) => (
+                <Grid item xs={2} key={index}>
+                  <Item index={index}></Item>
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+        </div>
       </header>
     </div>
   );
