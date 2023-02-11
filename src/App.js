a6fb2599fbe298645c46fe82b2fb12e1d14e930b
NathanDuggal <39681791+NathanDuggal@users.noreply.github.com>
2023-02-11 01:55:43 -0500

    poggers mathematics

--- src/App.js
+++ src/App.js
@@ -7,12 +7,25 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 
+const NUM_COLUMNS = 4;
+const COLUMNS_PER_BOX = 1;
+const NUM_BOXES = 28;
+
 const Item = function Item(props) {
   const onClick = event => {
     console.log(props.index);
+    let thisItem = document.getElementById(String(props.index));
+    console.log(thisItem.style.backgroundColor);
+    let toggle = String(thisItem.style.backgroundColor) === "rgb(255, 255, 255)" || String(thisItem.style.backgroundColor) == "";
+    for (let i = Number(props.index) % NUM_COLUMNS; i < NUM_BOXES; i += NUM_COLUMNS) {
+      document.getElementById(String(i)).style.backgroundColor = "#FFFFFF";
+    }
+    if (toggle) {
+      thisItem.style.backgroundColor = "#000000"; 
+    }
   }
 
-  return <div className='item' onClick={onClick}>{props.index}</div>
+  return <div id={props.index} className='item' onClick={onClick}>{props.index}</div>
 }
 
 function select(e) {
@@ -39,9 +52,9 @@ function App() {
         <br></br>
         <div className='gridBox'>
           <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={0} columns={8}>
-              {Array.from(Array(28)).map((_, index) => (
-                <Grid item xs={2} key={index}>
+            <Grid container spacing={0} columns={NUM_COLUMNS}>
+              {Array.from(Array(NUM_BOXES)).map((_, index) => (
+                <Grid item xs={COLUMNS_PER_BOX} key={index}>
                   <Item index={index}></Item>
                 </Grid>
               ))}
